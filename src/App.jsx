efd1c49bf565fae6bdeb8374661805b26808d429
Nick Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2023-09-01 15:45:42 -0400

    Removed unused imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
 import MyWork from "./pages/myWork";
@@ -10,18 +8,17 @@ import Modal from "./Functions/functions";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navbar/>}>
-            <Route index element={<Home/>}></Route>
-            <Route path="MyWork" element={<MyWork />} />
-            <Route path="AboutMe" element={<AboutMe />} />
-          </Route>
-        </Routes>
-        <Modal/>
-      </BrowserRouter>
-
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />}></Route>
+          <Route path="MyWork" element={<MyWork />} />
+          <Route path="AboutMe" element={<AboutMe />} />
+        </Route>
+      </Routes>
+      <Modal />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
