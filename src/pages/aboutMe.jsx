2a8a57ada8b9b3abf5b17b4433e2fd6522262368
RemusofRome <134432814+RemusofRome@users.noreply.github.com>
2023-09-01 15:04:56 -0400

    changed photo

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe () {
     return (
         <div id="aboutMeContainer">
         <h1>About Me</h1>
-        <img id="profileImg" src="src\Pictures\Profile.jpg"></img>
+        <img id="profileImg" src="src\Pictures\Profile.png"></img>
         <h2>Brandon Flach</h2>
         <p>I like long walks on the beach</p>
         <ul>
